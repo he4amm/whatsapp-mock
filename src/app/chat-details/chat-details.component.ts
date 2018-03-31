05d95ce4078d7db017008dd658e43c4d55d59542
Hesham Mamdouh <he4amm@gmail.com>
2018-03-31 14:27:40 +0200

    [add] functionality to make chat scrolled bottom by default & [fixes] chat details stikcy date

--- src/app/chat-details/chat-details.component.ts
+++ src/app/chat-details/chat-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
@@ -6,13 +6,27 @@ import * as moment from 'moment';
   templateUrl: './chat-details.component.html',
   styleUrls: ['./chat-details.component.css']
 })
-export class ChatDetailsComponent implements OnInit {
+export class ChatDetailsComponent implements OnInit, OnChanges {
   @Input('chatDetails') chatDetails: any;
   @Output() mobileBackAction: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes) {
+    // scroll bottom every time chat details change
+    if (changes['chatDetails']) {
+      this.scrollToBottom();
+    }
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
   }
 
   isFirstInRowSender(index) {
