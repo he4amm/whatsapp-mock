d652dcdff211f9bb4d5993c795f4e95bffc95656
Hesham Mamdouh <he4amm@gmail.com>
2018-04-01 16:29:11 +0200

    [add] functionality to send image in chat and view imgs in chat

--- src/app/chat-details/chat-details.component.ts
+++ src/app/chat-details/chat-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
@@ -6,25 +6,26 @@ import * as moment from 'moment';
   templateUrl: './chat-details.component.html',
   styleUrls: ['./chat-details.component.css']
 })
-export class ChatDetailsComponent implements OnInit, OnChanges {
+export class ChatDetailsComponent implements OnInit, AfterViewChecked {
   @Input('chatDetails') chatDetails: any;
   @Output() mobileBackAction: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() addMessageInChat: EventEmitter<any> = new EventEmitter<any>();
   @ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
-  message: string;
+  message: String = '';
+  _prevChatHeight: Number = 0;
 
   constructor() {
-    this.message = '';
   }
 
   ngOnInit() {
   }
 
-  ngOnChanges(changes) {
-    // scroll bottom every time chat details change
-    if (changes['chatDetails']) {
-      this.scrollToBottom();
+  ngAfterViewChecked() {
+    if (this.canScrollDown()) {
+      setTimeout(() => {
+        this.scrollToBottom();
+      }, 0);
     }
   }
 
@@ -34,9 +35,19 @@ export class ChatDetailsComponent implements OnInit, OnChanges {
     this.openPopup = fn;
   }
 
+  canScrollDown(): boolean {
+    /* compares prev and current scrollHeight */
+
+    const can = (this._prevChatHeight !== this.myScrollContainer.nativeElement.scrollHeight);
+
+    this._prevChatHeight = this.myScrollContainer.nativeElement.scrollHeight;
+
+    return can;
+  }
+
   scrollToBottom(): void {
     try {
-      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+      this.myScrollContainer.nativeElement.scrollTop = this._prevChatHeight;
     } catch (err) { }
   }
 
@@ -59,27 +70,65 @@ export class ChatDetailsComponent implements OnInit, OnChanges {
     });
   }
 
-  sendMessage() {
+  sendMessage(content, type) {
     // if message is empty terminate
-    if (!this.message) { return; }
+    if (!this.message && type === 'text') { return; }
 
-    this.addMessageInChat.next([
-      {
-        sender: 'me',
-        date: new Date,
-        text: this.message,
-        status: 'sent',
-        attachment: {
-          type: '',
-          link: ''
+    if (type === 'text') {
+      this.addMessageInChat.next([
+        {
+          sender: 'me',
+          date: new Date,
+          text: content,
+          status: 'sent',
+          attachment: {
+            type: '',
+            link: ''
+          }
+        },
+        {
+          id: this.chatDetails.id
         }
-      },
-      {
-        id: this.chatDetails.id
-      }
-    ]);
+      ]);
+      this.message = '';
+    } else if (type === 'img') {
+      this.addMessageInChat.next([
+        {
+          sender: 'me',
+          date: new Date,
+          text: '',
+          status: 'sent',
+          attachment: {
+            type: 'image',
+            link: content
+          }
+        },
+        {
+          id: this.chatDetails.id
+        }
+      ]);
+    }
 
-    this.message = '';
+  }
+
+  onSelectImg(event) {
+    const reader = new FileReader();
+    const createPharmacy = this;
+    const _this = this;
+
+    // validate format
+    const format = event.target.files[0].name.split('.').pop();
+    if (format !== 'png' && format !== 'jpg' && format !== 'jpeg') {
+      alert('invalid format to upload');
+      return;
+    }
+
+    reader.onload = function (e) {
+      const src = e.target['result'];
+      _this.sendMessage(src, 'img');
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
   }
 
   goBack() {
